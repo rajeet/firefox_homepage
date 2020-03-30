9f8c29bbb38d4732b890f9797fbf08588535ba43
rajeet <mangratirajit@gmail.com>
2020-03-30 13:19:54 +0545

    latest update

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,5 @@
 <script>
 window.onload = () => {
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 document.getElementById("clock").innerHTML = getTime();
           // Set clock interval to tick clock
           setInterval( () => {
