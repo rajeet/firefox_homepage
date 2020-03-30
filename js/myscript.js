7e69f46b8b8505676ffe32bac7ed8b3909605406
rajeet <mangratirajit@gmail.com>
2020-03-30 13:11:47 +0545

    changes to weather

---
+++ js/myscript.js
@@ -0,0 +1,35 @@
+<script>
+window.onload = () => {
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+document.getElementById("clock").innerHTML = getTime();
+          // Set clock interval to tick clock
+          setInterval( () => {
+              document.getElementById("clock").innerHTML = getTime();
+          },100);
+}
+
+function getTime() {
+          let date = new Date(),
+              min = date.getMinutes(),
+              sec = date.getSeconds(),
+              hour = date.getHours();
+
+          return "" +
+              (hour < 10 ? ("0" + hour) : hour) + ":" +
+              (min < 10 ? ("0" + min) : min) + ":" +
+              (sec < 10 ? ("0" + sec) : sec);
+      }
+
+      function search(e) {
+                if (e.keyCode == 13) {
+                    var val = document.getElementById("search-field").value;
+                    window.open("https://google.com/search?q=" + val);
+                }
+            }
+      function you_search(e){
+        if (e.keyCode == 13) {
+            var val = document.getElementById("you_search-field").value;
+            window.open("https://www.youtube.com/results?search_query=" + val);
+        }
+      }
+</script>
